2d87a36a60c26ec8d208dbe7c9338f3544771659
Fonvi <Fonvisinoff@yandex.ru>
2021-03-05 12:25:49 +0300

    EncoderByFonvi

---
+++ Encoder/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Encoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1898, 400);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(0, 624);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1898, 400);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(0, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(950, 224);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Encode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(948, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(950, 224);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Encoder/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encoder
+{
+    public partial class Form1 : Form
+    {    
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        Encoding encoding = Encoding.UTF8;        
+        FileStream FS;
+        byte[] EncryptedData;
+        byte[] DecryptedData;
+        byte[] data = new byte[1024];
+        public static RSAParameters publicKey, privateKey;
+        public string CD = Environment.CurrentDirectory;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {                
+                data = encoding.GetBytes(textBox1.Text);
+                EncryptedData = RSA.Encrypt(data, false);
+                textBox2.Text = Convert.ToBase64String(EncryptedData);
+                textBox1.Clear();
+            } else { MessageBox.Show("Введите текст!"); textBox1.Focus(); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                DecryptedData = RSA.Decrypt(EncryptedData, false);
+                textBox1.Text = encoding.GetString(DecryptedData);
+                textBox2.Clear();
+            }
+            else { MessageBox.Show("Введите текст!"); textBox2.Focus(); }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(CD+"\\publicKey.xml") == true && File.Exists(CD+"\\privateKey.xml") == true)
+            {
+                using (StreamReader sr = new StreamReader(CD + "\\publicKey.xml"))
+                { RSA.FromXmlString(sr.ReadToEnd()); sr.Close(); }
+                using (StreamReader sr = new StreamReader(CD + "\\privateKey.xml"))
+                { RSA.FromXmlString(sr.ReadToEnd()); sr.Close(); }
+            }
+            else
+            {
+                FS = File.Create(CD + "\\publicKey.xml"); FS.Close();
+                FS = File.Create(CD + "\\privateKey.xml"); FS.Close();
+                using (StreamWriter sw = new StreamWriter(CD + "\\publicKey.xml", false, encoding))
+                {sw.WriteLine(RSA.ToXmlString(false)); sw.Close();}
+                using (StreamWriter sw = new StreamWriter(CD + "\\privateKey.xml", false, encoding))
+                { sw.WriteLine(RSA.ToXmlString(true)); sw.Close();}
+                using (StreamReader sr = new StreamReader(CD + "\\publicKey.xml"))
+                {RSA.FromXmlString(sr.ReadToEnd()); sr.Close();}
+                using (StreamReader sr = new StreamReader(CD + "\\privateKey.xml"))
+                {RSA.FromXmlString(sr.ReadToEnd()); sr.Close();}
+            }
+        }
+    }
+}
